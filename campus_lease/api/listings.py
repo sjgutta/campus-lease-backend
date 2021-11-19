9af880d8deb588e1b38e24b3ff1ac3671443043f
Sajan Gutta <gutta@usc.edu>
2021-11-18 17:30:49 -0800

    Implementing endpoints

--- campus_lease/api/listings.py
+++ campus_lease/api/listings.py
@@ -1,4 +1,5 @@
-from flask import Blueprint
+from flask import Blueprint, request
+from campus_lease.models.listing import Listing
 
 listings_bp = Blueprint('api', __name__)
 
@@ -6,10 +7,35 @@ listings_bp = Blueprint('api', __name__)
 # Returns list of all startups
 @listings_bp.route('/listings/submit', methods=['POST'])
 def add_listing():
-    pass
+    data = request.form
+
+    name = data.get("name")
+    address = data.get("address")
+    city = data.get("city")
+    state = data.get("state")
+    zip_code = data.get("zip_code")
+    image_url = data.get("image_url")
+    email = data.get("email")
+    description = data.get("description")
+    amenities = data.get("amenities")
+
+    attributes = [name, address, city, state, zip_code, image_url, email, description, amenities]
+
+    for attribute in attributes:
+        if not attribute:
+            return {"error": "missing a field!"}
+
+    Listing.create(name=name, address=address, city=city, state=state, zip_code=zip_code, image_url=image_url,
+                   email=email, description=description, amenities=amenities)
+
+    return {"success": f"Create listing with name {name}!"}
 
 
 # Returns list of startups with similar names
 @listings_bp.route('/listings', methods=['GET'])
-def get_startups_name():
-    return "Hello!"
+def get_listings():
+    listings = Listing.select()
+    data = [listing.to_json() for listing in listings]
+    return {
+        "listings": data
+    }
